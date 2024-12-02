b7fcf6b79cf22f57f1a719115a52cd3c3128c333
puuueiei <pukunggamer123@gmail.com>
2024-12-03 01:50:17 +0700

    Add fade in

--- script.js
+++ script.js
@@ -78,3 +78,11 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+const cursor = document.createElement('div');
+cursor.classList.add('cursor');
+document.body.appendChild(cursor);
+
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = `${e.pageX}px`;
+    cursor.style.top = `${e.pageY}px`;
+});
